74bf702aabbfb8f478d3e9a6de665d821c2966c7
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2024-08-17 13:56:10 +0200

    Update ICM20948_13_FIFO_continuous.ino

--- examples/ICM20948_13_FIFO_continuous/ICM20948_13_FIFO_continuous.ino
+++ examples/ICM20948_13_FIFO_continuous/ICM20948_13_FIFO_continuous.ino
@@ -36,6 +36,7 @@ const int intPin = 2;
 volatile bool womEvent = false;
 
 void setup() {
+  //delay(2000); // maybe needed for some MCUs, in particular for startup after power off
   Wire.begin();
   Serial.begin(115200);
   while(!Serial) {}
