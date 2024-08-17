c921d251f8b98f2ff64b0898dcb79144687de8ae
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2024-08-17 13:55:22 +0200

    Update ICM20948_11_fsync_interrupt.ino

--- examples/ICM20948_11_fsync_interrupt/ICM20948_11_fsync_interrupt.ino
+++ examples/ICM20948_11_fsync_interrupt/ICM20948_11_fsync_interrupt.ino
@@ -30,6 +30,7 @@ const int intPin = 2;
 volatile bool fsyncEvent = false;
 
 void setup() {
+  //delay(2000); // maybe needed for some MCUs, in particular for startup after power off
   Wire.begin();
   Serial.begin(115200);
   while(!Serial) {}
